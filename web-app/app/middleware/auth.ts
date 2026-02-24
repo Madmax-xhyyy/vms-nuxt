84841701f7008f49ec50f0281d4b77f9fe2430c7
mdmx <143160694+Madmax-xhyyy@users.noreply.github.com>
2026-02-24 14:35:35 +0800

    Update auth.ts

--- web-app/app/middleware/auth.ts
+++ web-app/app/middleware/auth.ts
@@ -2,12 +2,11 @@ export default defineNuxtRouteMiddleware(async () => {
   // Ensure middleware runs only on the client side
   if (import.meta.server) return;
 
-  const { cookieConfig } = useRuntimeConfig().public;
-
-  const sid = useCookie("sid", cookieConfig).value;
+  const sid = useCookie("sid").value;
 
   if (!sid) {
     // Redirect to login page if no access token
     return navigateTo({ name: "index" });
   }
+
 });
